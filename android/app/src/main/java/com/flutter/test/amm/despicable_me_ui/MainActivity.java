eb56ff6c333ab01b851760ecee21c922068951fb
Aye Myat Mon <ayemyat.se@gmail.com>
2019-09-03 16:34:45 +0630

    first commit

---
+++ android/app/src/main/java/com/flutter/test/amm/despicable_me_ui/MainActivity.java
@@ -0,0 +1,13 @@
+package com.flutter.test.amm.despicable_me_ui;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
