03f1548e0ce9d28372224a0183b13449795fd675
Aye Myat Mon <ayemyat.se@gmail.com>
2019-09-03 17:37:25 +0630

    second commit

---
+++ lib/pages/character_listing_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:despicable_me_ui/styleguide.dart';
+import 'package:despicable_me_ui/widgets/character_widget.dart';
+
+class CharacterListingScreen extends StatefulWidget {
+  @override
+  _CharacterListingScreenState createState() => _CharacterListingScreenState();
+}
+
+class _CharacterListingScreenState extends State<CharacterListingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: Icon(Icons.arrow_back_ios, color: Colors.black,),
+        actions: <Widget>[
+          Padding(
+              padding: const EdgeInsets.only(right: 16),
+            child: Icon(Icons.search),
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 32.0, top: 8.0),
+            child: RichText(
+                text: TextSpan(
+                  children: [
+                    TextSpan(text: "Despicable Me", style: AppTheme.display1),
+                    TextSpan(text: "\n"),
+                    TextSpan(text: "Character", style: AppTheme.display2),
+                  ]
+                )
+            ),
+          ),
+          Expanded(
+              child: CharacterWidget()
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/character_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';class
+
+CharacterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
